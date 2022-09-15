3145096db8fe18238c5d7023712047ef09fc7701
Batel Haiylo <inpo686@gmail.com>
2022-09-15 23:13:18 +0300

    extra on the card

--- src/components/featurs/Card.component.jsx
+++ src/components/featurs/Card.component.jsx
@@ -2,7 +2,7 @@ import {StyledCard} from '../styles/Card.style'
 
 export default function Card({item:{id,title,body,image}}) {
     return ( 
-        <StyledCard>
+        <StyledCard layout={id % 2 === 0 && 'row-reverse'}>
             <div>
                 <h2>{title}</h2>
                 <p>{body}</p>
